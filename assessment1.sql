8d7594f566d5d507a3d70fc7de1d7cb4997d6f0d
rg7606 <94630493+rg7606@users.noreply.github.com>
2023-05-05 18:04:57 +0530

    Update assessment1.sql

--- assessment1.sql
+++ assessment1.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE sales;
-  USE sales;
-  CREATE TABLE orders (order_id int primary key,customer_id int,order_date DATE, order_total decimal(10,2));
-  INSERT INTO sales(order_id,customer_id,order_date,order_total) values (1,13,30,1200);
-  INSERT INTO sales(order_id,customer_id,order_date,order_total) values (2,14,31,1800);
-  INSERT INTO sales(order_id,customer_id,order_date,order_total) values (3,15,1,2250;
-  
+USE sales;
+CREATE TABLE orders (order_id int primary key, customer_id int, order_date DATE, order_total decimal(10,2));
+INSERT INTO orders(order_id, customer_id, order_date, order_total) values (1, 13, '2022-04-30', 1200);
+INSERT INTO orders(order_id, customer_id, order_date, order_total) values (2, 14, '2022-05-01', 1800);
+INSERT INTO orders(order_id, customer_id, order_date, order_total) values (3, 15, '2022-05-02', 2250);
