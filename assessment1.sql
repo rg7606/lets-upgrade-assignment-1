ecbc0f85e7c6c8fa53d03b7a9c886ad60adfc652
rg7606 <94630493+rg7606@users.noreply.github.com>
2023-05-05 17:59:05 +0530

    Add files via upload

---
+++ assessment1.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE sales;
+  USE sales;
+  CREATE TABLE orders (order_id int primary key,customer_id int,order_date DATE, order_total decimal(10,2));
+  INSERT INTO sales(order_id,customer_id,order_date,order_total) values (1,13,30,1200);
+  INSERT INTO sales(order_id,customer_id,order_date,order_total) values (2,14,31,1800);
+  INSERT INTO sales(order_id,customer_id,order_date,order_total) values (3,15,1,2250;
+  
